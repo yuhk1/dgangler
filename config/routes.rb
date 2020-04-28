d0c827d0415ce7254db9f8acf2cfe8825d085366
yuhk1 <yuhk1.nakamura7@gmail.com>
2020-04-28 10:14:54 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
-  root to: "toppages#index"
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  root to: 'toppages#index'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
